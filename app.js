0cc53216ff3fc32dfd6577f1a002615b8a8b6254
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-02 19:43:16 -0600

    forgot to commit still fixing event listener issues.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// import functions and grab DOM elements
-import { renderIngredients, renderMeals } from './utils.js';
 
+// import functions and grab DOM elements
+import { renderIngredientLI, renderMealLI } from './utils.js';
 //grabbing form, ingredients, and meals(recipe)
 const form = document.getElementById('add-ingredient'); // grab form
 const ingredientsList = document.getElementById('ingredient-list'); // list of ingredients
@@ -13,7 +13,7 @@ const saveMeal = document.getElementById('save-meal');
 
 //let states the ingredients combined = meals and added to total meal 
 let ingredients = [];
-let recipes = []; 
+let meals = [];
 
 // set event listeners 
 // IMPURE RENDER FUNCTIONS 
@@ -21,45 +21,55 @@ let recipes = [];
 function renderMeals() {
   mealList.textContent = '';
   for (let meal of meals) {
-      const li = renderMeals(meal);
-      mealList.append(li);
+    const li = renderMealLI(meal);
+    mealList.append(li);
   }
 }
 
 function renderIngredients() {
   ingredientsList.textContent = '';
   for (let item of ingredients) {
-      const li = renderIngredients(item);
-      ingredientsList.appendChild(li);
+    const li = renderIngredientLI(item);
+    ingredientList.appendChild(li);
   }
 }
 
-function resetMeals() {
-  meal = [];
-  mealList.textContent = '';
-}
+
+// function resetMeals() {
+//   meal = [];
+//   mealList.textContent = '';
+// }
 
 form.addEventListener('submit', (e) => {
-  // Step 1 - add code to track each submission to the stats
-  // Create an object to "model" you statistic like the following:
-  // { player: 'Bob', points: 2 }
-  // Hint -- create the object from the form, push it onto the stats array, then call renderStats
+  e.preventDefault();
+  const data = new FormData(form);
+  const item = {
+    ingredient: data.get('ingredient'),
+    qty: data.get('qty'),
+    measurement: data.get('measurement'),
+    calories: Number(data.get('calories')),
+  };
+  ingredients.push(item);
+  renderIngredients();
+  form.reset();
 });
 
 remove.addEventListener('click', () => {
-  // Step 2 -- add code to allow users to remove the most recent stat
-  // Hint -- how do you remove an element from an array?
-  // Hint -- how can we rerender the stats using a function above?
+  //Step 2 -- add code to allow users to remove the most recent stat
+  //   // Hint -- how do you remove an element from an array?
+  //   // Hint -- how can we rerender the stats using a function above?
+  ingredients.pop();
+  renderIngredients();
 });
 
-save.addEventListener('click', () => {
-  // Step 3 - add code to allow users to save the state
-  // Loop through the list of stats and add up the total points scored
-  // Create a new object with the game number and the total points
-  // { number: games.length + 1, totalPoints: totalPoints }
-  // Push the new object onto the games array then call renderGames
-  // reset the stats with resetStats
-});
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// save.addEventListener('click', () => {
+//   // Step 3 - add code to allow users to save the state
+//   // Loop through the list of stats and add up the total points scored
+//   // Create a new object with the game number and the total points
+//   // { number: games.length + 1, totalPoints: totalPoints }
+//   // Push the new object onto the games array then call renderGames
+//   // reset the stats with resetStats
+// });
+//   // get user input
+//   // use user input to update state
+//   // update DOM to reflect the new state
