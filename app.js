9352135897d41eb6f0dcd096eb3d7e5a4614d8a2
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-07 11:17:52 -0600

    working on event listeners no error but not working still

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 
 // import functions and grab DOM elements
 import { renderIngredientLI, renderMealLI } from './utils.js';
@@ -15,9 +16,22 @@ const saveMeal = document.getElementById('save-meal');
 let ingredients = [];
 let meals = [];
 
-// set event listeners 
-// IMPURE RENDER FUNCTIONS 
+// set event listeners
+// IMPURE RENDER FUNCTIONS
 // YOUR CODE MUST CALL THESE FUNCTIONS
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  const item = {
+    ingredients: data.get('ingredient'),
+    qty: data.get('qty'),
+    measurement: data.get('measurement'),
+  };
+  ingredients.push(item);
+  renderIngredients();
+  form.reset();
+});
+
 function renderMeals() {
   mealList.textContent = '';
   for (let meal of meals) {
@@ -35,25 +49,6 @@ function renderIngredients() {
 }
 
 
-// function resetMeals() {
-//   meal = [];
-//   mealList.textContent = '';
-// }
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const data = new FormData(form);
-  const item = {
-    ingredient: data.get('ingredient'),
-    qty: data.get('qty'),
-    measurement: data.get('measurement'),
-    calories: Number(data.get('calories')),
-  };
-  ingredients.push(item);
-  renderIngredients();
-  form.reset();
-});
-
 remove.addEventListener('click', () => {
   //Step 2 -- add code to allow users to remove the most recent stat
   //   // Hint -- how do you remove an element from an array?
@@ -62,14 +57,26 @@ remove.addEventListener('click', () => {
   renderIngredients();
 });
 
-// save.addEventListener('click', () => {
-//   // Step 3 - add code to allow users to save the state
-//   // Loop through the list of stats and add up the total points scored
-//   // Create a new object with the game number and the total points
-//   // { number: games.length + 1, totalPoints: totalPoints }
-//   // Push the new object onto the games array then call renderGames
-//   // reset the stats with resetStats
-// });
+saveMeal.addEventListener('click', () => {
+  //   // Step 3 - add code to allow users to save the state
+  const name = meal.value;
+  const count = ingredients.length;
+  //   // Loop through the list of stats and add up the total points scored
+  meals.push({ name, count });
+  //   // Create a new object with the game number and the total points
+  //   // { number: games.length + 1, totalPoints: totalPoints }
+  //   // Push the new object onto the games array then call renderGames
+  //   // reset the stats with resetStats
+  renderMeals();
+  resetIngredients();
+});
 //   // get user input
 //   // use user input to update state
 //   // update DOM to reflect the new state
+function resetMeals() {
+  mealList.textContent = '';
+}
+
+function resetIngredients() {
+  ingredientsList.textContent = '';
+}
