de66a5b9313f52ad2022b7c532b07fa08be19cc3
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-08 11:07:24 -0600

    saving code from last night

--- app.js
+++ app.js
@@ -20,63 +20,64 @@ let meals = [];
 // IMPURE RENDER FUNCTIONS
 // YOUR CODE MUST CALL THESE FUNCTIONS
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const data = new FormData(form);
-    const item = {
-        ingredients: data.get('ingredient'),
-        qty: data.get('qty'),
-        measurement: data.get('measurement'),
-    };
-    ingredients.push(item);
-    renderIngredients();
-    form.reset();
+  e.preventDefault();
+  const data = new FormData(form);
+  const item = {
+    ingredients: data.get('ingredient'),
+    qty: data.get('qty'),
+    measurement: data.get('measurement'),
+    calories: Number(data.get('calories')),
+  };
+  ingredients.push(item);
+  renderIngredients();
+  form.reset();
 });
 
 function renderMeals() {
-    mealList.textContent = '';
-    for (let meal of meals) {
-        const li = renderMealLI(meal);
-        mealList.append(li);
-    }
+  mealList.textContent = '';
+  for (let meal of meals) {
+    const li = renderMealLI(meal);
+    mealList.append(li);
+  }
 }
 
 function renderIngredients() {
-    ingredientsList.textContent = '';
-    for (let item of ingredients) {
-        const li = renderIngredientLI(item);
-        ingredientsList.appendChild(li);
-    }
+  ingredientsList.textContent = '';
+  for (let item of ingredients) {
+    const li = renderIngredientLI(item);
+    ingredientsList.appendChild(li);
+  }
 }
 
 
 remove.addEventListener('click', () => {
   //Step 2 -- add code to allow users to remove the most recent stat
   //   // Hint -- how do you remove an element from an array?
-    ingredients.pop();
+  ingredients.pop();
   //   // Hint -- how can we rerender the stats using a function above? 
-    renderIngredients();
+  renderIngredients();
 });
 
 saveMeal.addEventListener('click', () => {
   //   // Step 3 - add code to allow users to save the state
-    const name = meal.value;
-    const count = ingredients.length;
+  const name = meal.value;
+  const count = ingredients.length;
   //   // Loop through the list of stats and add up the total points scored
-    meals.push({ name, count });
+  meals.push({ name, count });
   //   // Create a new object with the game number and the total points
   //   // { number: games.length + 1, totalPoints: totalPoints }
   //   // Push the new object onto the games array then call renderGames
   //   // reset the stats with resetStats
-    renderMeals();
-    resetIngredients();
+  renderMeals();
+  resetIngredients();
 });
 //   // get user input
 //   // use user input to update state
 //   // update DOM to reflect the new state
 function resetMeals() {
-    mealList.textContent = '';
+  mealList.textContent = '';
 }
 
 function resetIngredients() {
-    ingredientsList.textContent = '';
+  ingredientsList.textContent = '';
 }
