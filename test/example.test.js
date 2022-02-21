4d4304aae423607c0870c10587e69a27b53dd648
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-20 22:42:31 -0600

    adding tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,7 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { renderMeals } from '../utils';
+import { renderIngredientLI, renderMealLI } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -11,7 +11,21 @@ test('renderMeal will create a list', (expect) => {
     const expected = `<li>Meal: Sandwich (3) ingredients</li>`;
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderMeals({ name: 'Sandwich', totalIngredients: 3 });
+    const actual = renderMealLI({ name: 'Sandwich', totalIngredients: 3 });
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
+
+test('return second test', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = '<li><p>undefined :</p><ul><li>undefined undefined undefined</li><li>undefined undefined undefined</li><li>undefined undefined undefined</li><li>undefined undefined undefined</li></ul></li>';
+
+    //Act
+    // Call the function you're testing and set the result to a const
+    const actual = renderIngredientLI({ ingredients: 'test' });
+
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
